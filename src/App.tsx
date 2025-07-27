250e806be0eeb2b72ec9605652e29c1218a2653e
Nipun Verma <Nipunverma39@gmail.com>
2025-07-27 18:31:38 +0530

    init app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Weather } from "./components";
+
+function App() {
+  return (
+    <div className="w-screen flex justify-center items-center h-screen">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
